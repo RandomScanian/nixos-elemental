1c9a883ac241eda3cf2be3e6d26986724dd46b77
RandomScanian <randomscanian@protonmail.com>
2024-02-03 22:11:58 +0100

    Early: fix audio

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -6,9 +6,17 @@ let cfg = config.randomscanian.hardware.audio;
 in
 {
   options.randomscanian.hardware.audio = with types; {
-    enable = mkBoolOpt false "";
+    enable = mkBoolOpt false "Whether or not to enable audio";
   };
 
   config = mkIf cfg.enable {
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
 }
